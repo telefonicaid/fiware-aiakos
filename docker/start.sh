d4afc2d95cfc7b0ef02a72f6861388d3394421d8
visitor <henar.munozfrutos@telefonica.com>
2016-04-13 09:10:37 +0200

    update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,7 @@ if test -r /etc/redhat-release; then
 else
    apt-get -y -q install dpkg-dev debhelper devscripts
 fi
-export RPM_FILE=$(find  -name "*.rpm")
 #  Generate package
 tools/build/package.sh
+export RPM_FILE=`find  -name "*.rpm"`
 uploadPkg.py --os-username=$OS_USERNAME_VALUE --os-password=$OS_PASSWORD_VALUE --os-tenant-name=$OS_TENANT_NAME_VALUE $RPM_FILE
