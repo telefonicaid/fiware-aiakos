dd473a6fe8e425b07c8610af36952f6845e0571b
visitor <henar.munozfrutos@telefonica.com>
2016-05-04 12:43:12 +0200

    update

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,5 @@
+#export RPM_FILE=`find  -name "*.rpm"`
+#rpm -i $RPM_FILE
 sed -i -e "s/{ADM_TENANT_NAME}/${ADM_TENANT_NAME}/" /etc/sysconfig/fiware-aiakos.yml
 sed -i -e "s/{ADM_PASSWORD}/${ADM_PASSWORD}/" /etc/sysconfig/fiware-aiakos.yml
 sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" /etc/sysconfig/fiware-aiakos.yml
